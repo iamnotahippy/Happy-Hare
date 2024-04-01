0749fccd54a3dbaad190a8ec769772a92c5be9f6
iamnotahippy <iamnotahippy@gmail.com>
2024-04-01 16:44:38 -0700

    Moved danger options

--- extras/mmu_toolhead.py
+++ extras/mmu_toolhead.py
@@ -28,9 +28,9 @@ class MmuToolHead(toolhead.ToolHead, object):
 
         self.printer = config.get_printer()
         self.reactor = self.printer.get_reactor()
-        self.all_mcus = [m for n, m in self.printer.lookup_objects(module='mcu')]
         self.danger_options = self.printer.lookup_object("danger_options")
-
+        self.all_mcus = [m for n, m in self.printer.lookup_objects(module='mcu')]
+       
         self.mcu = self.all_mcus[0]
         if hasattr(toolhead, 'LookAheadQueue'):
             self.lookahead = toolhead.LookAheadQueue(self) # Happy Hare: Use base class LookAheadQueue
